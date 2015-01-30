685301361374f4da76617e36154bd72d0e3fc858
Van Doan <vdoan09@gmail.com>
2015-01-29 23:42:43 -0500

    random image

---
+++ randome_image.php
@@ -0,0 +1,6 @@
+<?php 
+$images = ('kinkakuji', 'maiko', 'maiko_phone', 'monk', 'fountains', 'ryoanji', 'menu', 'bain');
+
+$i - rand(0, count($image)-1);
+
+$selectedImage = "images/($images[$i]).jpg";
